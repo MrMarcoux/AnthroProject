6e4928c930ce729f78c148268039b5552ae5f56e
samsas556 <samuel.brassard@gmail.com>
2020-08-16 11:46:41 -0400

    define skill and upcomingskill

--- src/models/skill.ts
+++ src/models/skill.ts
@@ -1,3 +1,31 @@
 export class Skill {
+
+    name : string;
+    level : number;
+
+    /**constructor */
+    constructor(name : string , level : number){
+        this.name = name;
+        this.level = level;
+    }
+    
+    /**getter */
+    public getName() : string {
+        return this.name;
+    }
+
+    public getLevel() : number {
+        return this.level;
+    }
+    
+    /**setter */
+    public setName(name : string) {
+        this.name = name;
+    }
+
+    public setLevel(level : number) {
+        this.level = level;
+    }
+    
     
 }
--- src/models/upcomingskill.ts
+++ src/models/upcomingskill.ts
@@ -1,5 +1,36 @@
 import { Skill } from './skill';
 
 export class UpcomingSkill extends Skill {
+    
+    aquisitionDate : Date;
+
+    constructor(name : string , level : number , date : Date){
+
+        super(name,level);
+        this.aquisitionDate = date;
+    }
+
+    /**getter */
+    public getAquisitionDate() : Date {
+        return this.aquisitionDate;
+    }
+
+    /**
+     * @returns true if the skill is aquired
+     */
+    isAcquierd() : boolean{
+
+        const today = new Date();
+        
+        if(this.aquisitionDate.getTime() <= today.getTime()){
+            return true;
+        }
+        return false;
+    }
+
+    /**setter */
+    public setAquisitionDate(date : Date){
+        this.aquisitionDate = date;
+    }
 
 }
