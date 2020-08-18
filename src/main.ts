3fdf62563ff201667827e8192b3851d1e01accb7
William Marcoux <william.marcoux.98@gmail.com>
2020-08-17 23:12:31 -0400

    Cleared repo temporarily

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-import App from './App.vue';
-import VueRouter from 'vue-router';
-import router from './router';
-
-new Vue({
-  render: h => h(App),
-  router
-}).$mount("#app");
